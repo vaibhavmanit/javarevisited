13b455fa726c90788b1a84d067d1f3fe5565800c
vaibhavmanit <vaibhav.manit@gmail.com>
2015-06-23 16:13:40 +0530

    Create Squeak.java

---
+++ Squeak.java
@@ -0,0 +1,11 @@
+package oracle.util.snippetjava01.duck;
+
+public class Squeak implements QuackBehaviour {
+    public Squeak() {
+        super();
+    }
+
+    public void quack() {
+        System.out.println("Squeak");
+    }
+}
