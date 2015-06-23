14ec48ca38a02e777267398605257126cf7e29cf
vaibhavmanit <vaibhav.manit@gmail.com>
2015-06-23 16:11:09 +0530

    Create FlyRocketPower.java

---
+++ FlyRocketPower.java
@@ -0,0 +1,11 @@
+package oracle.util.snippetjava01.duck;
+
+public class FlyRocketPower implements FlyBehaviour {
+    public FlyRocketPower() {
+        super();
+    }
+
+    public void fly() {
+        System.out.println("I'm flying with rocket!");
+    }
+}
